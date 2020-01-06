cae9077b6db4186de5e3ee3bc924ddcc802173eb
Dusan Petrovic <dusanp93@icloud.com>
2020-01-06 19:23:34 +0100

    added: google fonts,  structure,  layout component, hoc Aux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
-import classes from "./App.module.css";
+import React, { Component } from "react";
 
-function App() {
-  return <div className={classes.App}></div>;
+import Layout from "./components/Layout/Layout";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
