e5345e3f76ba1c9c302fbe25b25a62722a487f41
Dusan Petrovic <dusanp93@icloud.com>
2020-02-04 23:36:18 +0100

    added order button, order summary

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -13,7 +13,7 @@ const burger = props => {
       return arr.concat(el);
     }, []);
   if (transformedIngredients.length === 0) {
-    transformedIngredients = <p>Please start adding ingredients</p>;
+    transformedIngredients = <strong>Please start adding ingredients</strong>;
   }
   console.log(transformedIngredients);
   return (
